c585f0ad3f162869f484dced66f4839c99b69b19
Kopaing12 <107101116+Kopaing12@users.noreply.github.com>
2022-07-20 13:57:44 +0630

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -161,8 +161,8 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
             ],
             [
                 InlineKeyboardButton(
-                    text=f"Projects Channel",
-                    url="https://t.me/josprojects"
+                    text=f"Main Korean Channel",
+                    url="https://t.me/MKSVIPLINK"
                 )
             ],
         ]
