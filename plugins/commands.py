0426b8cd884d4dd42dbffab08c4de2e2d51108b1
Kopaing12 <107101116+Kopaing12@users.noreply.github.com>
2022-07-20 12:24:58 +0630

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -92,7 +92,7 @@ async def start(client, message):
                 btn.append([InlineKeyboardButton("🔄 Try Again 👈 Tap me 🥰", callback_data=f"{pre}#{file_id}")])
         await client.send_message(
             chat_id=message.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**",
+            text="**🗣 မိတ်‌ဆွေကြည့်ချင်တဲ့ဇာတ်ကားကို ပို့ပေးဖို့အတွက် 👉🏻 Join Channel 👈🏻 ကို Join ထားဖို့လိုပါတယ်။ Channel လေးကို Join ပြီးရင်  Try Again 👈 Tap me လေးကို နှိပ်ပါ ပြီးရင် start လေးပြန်ပေါ်လာတာကို နှိပ်လိုက်ရင် 👌 ရပါပြီး။ \n\n🙈@KOPAING15 **",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode="markdown"
             )
