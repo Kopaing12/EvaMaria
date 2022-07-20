da9529cdfc1c3490447dee4b50789b0dafdc53ac
Kopaing12 <107101116+Kopaing12@users.noreply.github.com>
2022-07-20 11:55:57 +0630

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -93,7 +93,7 @@ async def start(client, message):
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode="markdown"
             )
-        return
+    return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
             InlineKeyboardButton('➕ Add me to your Chat ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
